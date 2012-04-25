0259e6f1cb5323c82b27838f7f3109fdb9aec8f5
Tyler Freedman <mail@tylerfreedman.com>
2012-04-25 03:55:19 -0400

    Implemented basic parsing

---
+++ DineSafer/DineSafe.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace DineSafer
+{
+    public class DineSafe
+    {
+        string ESTABLISHMENT_NAME;
+        string ESTABLISHMENTTYPE;
+        string ESTABLISHMENT_ADDRESS;
+        string ESTABLISHMENT_STATUS;
+        string INFRACTION_DETAILS;
+        string INFRACTION_DATE;
+        string SEVERITY;
+
+        public string Name
+        {
+            get { return ESTABLISHMENT_NAME; }
+            set { ESTABLISHMENT_NAME = value; }
+        }
+        public string FoodType
+        {
+            get { return ESTABLISHMENTTYPE; }
+            set { ESTABLISHMENTTYPE = value; }
+        }
+        public string Address
+        {
+            get { return ESTABLISHMENT_ADDRESS; }
+            set { ESTABLISHMENT_ADDRESS = value; }
+        }
+        public string Status
+        {
+            get { return ESTABLISHMENT_STATUS; }
+            set { ESTABLISHMENT_STATUS = value; }
+        }
+        public string Details
+        {
+            get { return INFRACTION_DETAILS; }
+            set { INFRACTION_DETAILS = value; }
+        }
+        public string Date
+        {
+            get { return INFRACTION_DATE; }
+            set { INFRACTION_DATE = value; }
+        }
+        public string Severity
+        {
+            get { return SEVERITY; }
+            set { SEVERITY = value; }
+        }
+    }
+}
--- DineSafer/MainPage.xaml.cs
+++ DineSafer/MainPage.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+using System.Windows.Resources;
 
 namespace DineSafer
 {
@@ -19,6 +23,23 @@ namespace DineSafer
         public MainPage()
         {
             InitializeComponent();
+            XDocument loadedData;
+
+            StreamResourceInfo xml = Application.GetResourceStream(new Uri("dinesafe.xml", UriKind.Relative));
+            loadedData = XDocument.Load(xml.Stream);
+
+            var data = from query in loadedData.Descendants("ROW")
+                       select new DineSafe
+                       {
+                           Name = (string)query.Element("ESTABLISHMENT_NAME"),
+                           FoodType = (string)query.Element("ESTABLISHMENTTYPE"),
+                           Address = (string)query.Element("ESTABLISHMENT_ADDRESS"),
+                           Status = (string)query.Element("ESTABLISHMENT_STATUS"),
+                           Details = (string)query.Element("INFRACTION_DETAILS"),
+                           Date = (string)query.Element("INFRACTION_DATE"),
+                           Severity = (string)query.Element("SEVERITY")
+                       };
+            listBox.ItemsSource = data;
         }
     }
 }
